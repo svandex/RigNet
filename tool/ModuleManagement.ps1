f94e70f952044dc5b73aac77244a9e3ef2dbd95e
Juncheng Qiu <svandex@outlook.com>
2020-04-01 17:02:59 +0800

    scripts for testing

--- tool/ModuleManagement.ps1
+++ tool/ModuleManagement.ps1
@@ -12,29 +12,26 @@ $isAdmin = (New-Object System.Security.Principal.WindowsPrincipal([System.Securi
 
 if ($isAdmin -eq $false) {
     Write-Output("[ps] The scirpt is not executed in Administrator role.")
-    exit
+    return
 } 
 
 if ($UninstallModuleName -ne "") {
     $ModuleList = Get-IISConfigSection -SectionPath "system.webServer/modules" | Get-IISConfigCollection
 
     foreach ($Module in $ModuleList) {
-        if (($Module).RawAttributes.Count -ne 0 -and ($Module).RawAttributes.name.CompareTo($UninstallModuleName) -eq 0) {
+        if ($Module.RawAttributes.Count -ne 0 -and $Module.RawAttributes.name.CompareTo($UninstallModuleName) -eq 0) {
             Write-Output("[ps] Module exist. Uninstalling module...")
-            $command = $env:SystemRoot + "\System32\inetsrv\appcmd uninstall module " + $UninstallModuleName+" >null"
+            $command = $env:SystemRoot + "\System32\inetsrv\appcmd uninstall module " + $UninstallModuleName+" |Out-Null"
             Invoke-Expression $command
-            exit
+            return
         }
     }
 
     Write-Output("[ps] No Module exist.")
 }
 
-
 if ($InstallModuleName -ne "" -and $ModulePath -ne "") {
-    $command = $env:SystemRoot + "\System32\inetsrv\appcmd install module /name:" + $InstallModuleName + " /image:" + $ModulePath+" >null"
+    $command = $env:SystemRoot + "\System32\inetsrv\appcmd install module /name:" + $InstallModuleName + " /image:" + $ModulePath+" /preCondition:bitness64"# |Out-Null"
     Invoke-Expression $command
     Write-Output("[ps] Module Installed.")
-}else{
-    Write-Output("[ps] Parameter number not enough.")
 }
--- tool/SendHttpToSever.bat
+++ tool/SendHttpToSever.bat
@@ -2,9 +2,11 @@
 
 if "%1"=="login" (curl -v -H "Content-Type: application/json" -d "{\"id\":\"G0000\",\"password\":\"saic\"}" localhost/login)
 
-if "%1"=="register" (curl -v -H "Connection: close" -d "{\"id\":\"G0001\",\"password\":\"asdf\",\"role\":\"te\",\"contact\":\"123456\",\"name\":\"hello\"}" localhost/register)
+if "%1"=="exist" (curl -v -H "Content-Type: application/json" -d "{\"id\":\"G0003\",\"password\":\"asdf\"}" localhost/exist)
 
-if "%1"=="data" (curl -v -H "Content-Type: application/json" -d "{\"database\":\"labwireless\",\"statement\":\"select * from `0用户信息`\",\"sessionId\":\"866c3c70-8c21-4cef-8fe2-97c81f0b9305\"}" localhost/sqlitedata)
+if "%1"=="regst" (curl -v -H "Connection: close" -d "{\"id\":\"G0015\",\"password\":\"asdf\",\"role\":\"te\",\"contact\":\"123456\",\"name\":\"hello\"}" localhost/regst)
+
+if "%1"=="data" (curl -v -H "Content-Type: application/json" -d "{\"database\":\"management\",\"statement\":\"select * from Test\",\"sessionid\":\"9e2f6b7d-07d5-4c2c-b21e-0146f9c1d306\"}" localhost/sqlite)
 
 if "%1"=="dataro" curl -v -H "Content-Type: application/json;charset=utf-8" -H "Connection: close" -d "{\"database\":\"labwireless\",\"readonly\":\"0\",\"statement\":\"select * from `0用户信息`\"}" localhost/sqlitedata
 
